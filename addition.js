ee41c19191894a5db39e70d039c12a8779526218
Vidit Pujara <viditpujara@outlook.com>
2022-09-08 11:26:59 +0530

    initial upload

---
+++ addition.js
@@ -0,0 +1,12 @@
+var a = 10;
+var b = 20;
+var c = a + b;
+console.log(c);
+
+console.log(`A value is: ${a} and B value is: ${c}`);
+
+if (c==30) {
+    console.log("Sum is 30");
+}else{
+    console.log("Sum is not 30");
+}
